85469105f4b34c2c7e842a39993c7e51ff12a648
RaphaelDizonn <joshrdizz@gmail.com>
2023-11-11 02:08:55 -0500

    Functional Colour Buttons
    The colour buttons now change the background colours and also the border colours
    
    removed universal css tag for background colour (overlaps the javascript functions) and instead added a background colour to body tag in css

---
+++ Project_ASR.js
@@ -0,0 +1,36 @@
+function black() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("QUICKQUILL").style.color = 'white';
+    document.getElementById("notepicker").style.borderColor = 'white';
+    document.getElementById("note").style.borderColor = 'white';
+    document.getElementById("header").style.borderColor = 'white';
+
+}
+
+function white() {
+    document.body.style.backgroundColor = "white";
+    document.getElementById("QUICKQUILL").style.color = 'black';
+    document.getElementById("notepicker").style.borderColor = 'black';
+    document.getElementById("note").style.borderColor = 'black';
+    document.getElementById("header").style.borderColor = 'black';
+
+
+}
+
+function pink() {
+    document.body.style.backgroundColor = "#ffc0b9";
+    document.getElementById("QUICKQUILL").style.color = 'white';
+    document.getElementById("notepicker").style.borderColor = 'white';
+    document.getElementById("note").style.borderColor = 'white';
+    document.getElementById("header").style.borderColor = 'white';
+
+}
+
+function blue() {
+    document.body.style.backgroundColor = "#b1eed0";
+    document.getElementById("QUICKQUILL").style.color = 'black';
+    document.getElementById("notepicker").style.borderColor = 'black';
+    document.getElementById("note").style.borderColor = 'black';
+    document.getElementById("header").style.borderColor = 'black';
+}
+
