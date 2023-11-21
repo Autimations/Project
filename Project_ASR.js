fc1b81e87ddc746ae00bcd2f022f1ec9be640a65
Sahil <panchal.sahil@outlook.com>
2023-11-21 01:19:42 +0000

    Just a small template, will finish tomorrow

--- Project_ASR.js
+++ Project_ASR.js
@@ -1,5 +1,11 @@
+/**
+ * Toggles between different themes for the application.
+ */
 function themePicker() {
-    let themeNumber = 0;;
+    // Current theme number
+    let themeNumber = 0;
+
+    // Switch between themes based on the current theme number
     switch (themeNumber) {
         case 0:
             setTheme('black', 'white');
@@ -20,8 +26,17 @@ function themePicker() {
     }
 }
 
+/**
+ * Sets the theme for the application.
+ *
+ * @param {string} bodyColor - The background color for the body.
+ * @param {string} textColor - The text color for various elements.
+ */
 function setTheme(bodyColor, textColor) {
+    // Set the background color of the body
     document.body.style.backgroundColor = bodyColor;
+
+    // Set text color for specific elements
     document.getElementById("QUICKQUILL").style.color = textColor;
     document.getElementById("notepicker").style.borderColor = textColor;
     document.getElementById("note").style.borderColor = textColor;
