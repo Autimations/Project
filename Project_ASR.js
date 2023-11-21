63ed6ab6467397f5058cde0b5dd2ed96bcd73531
Sahil <panchal.sahil@outlook.com>
2023-11-21 00:42:55 +0000

    Reworked

--- Project_ASR.js
+++ Project_ASR.js
@@ -1,42 +1,30 @@
-function black() {
-    document.body.style.backgroundColor = "black";
-    document.getElementById("QUICKQUILL").style.color = 'white';
-    document.getElementById("notepicker").style.borderColor = 'white';
-    document.getElementById("note").style.borderColor = 'white';
-    document.getElementById("header").style.borderColor = 'white';
-    document.getElementById("authors").style.color = 'white';
-
+function themePicker() {
+    let themeNumber = 0;;
+    switch (themeNumber) {
+        case 0:
+            setTheme('black', 'white');
+            themeNumber++;
+            break;
+        case 1:
+            setTheme('white', 'black');
+            themeNumber++;
+            break;
+        case 2:
+            setTheme('#ffc0b9', 'white');
+            themeNumber++;
+            break;
+        case 3:
+            setTheme('#b1eed0', 'black');
+            themeNumber = 0;
+            break;
+    }
 }
 
-function white() {
-    document.body.style.backgroundColor = "white";
-    document.getElementById("QUICKQUILL").style.color = 'black';
-    document.getElementById("notepicker").style.borderColor = 'black';
-    document.getElementById("note").style.borderColor = 'black';
-    document.getElementById("header").style.borderColor = 'black';
-    document.getElementById("authors").style.color = 'black';
-
-
-
+function setTheme(bodyColor, textColor) {
+    document.body.style.backgroundColor = bodyColor;
+    document.getElementById("QUICKQUILL").style.color = textColor;
+    document.getElementById("notepicker").style.borderColor = textColor;
+    document.getElementById("note").style.borderColor = textColor;
+    document.getElementById("header").style.borderColor = textColor;
+    document.getElementById("authors").style.color = textColor;
 }
-
-function pink() {
-    document.body.style.backgroundColor = "#ffc0b9";
-    document.getElementById("QUICKQUILL").style.color = 'white';
-    document.getElementById("notepicker").style.borderColor = 'white';
-    document.getElementById("note").style.borderColor = 'white';
-    document.getElementById("header").style.borderColor = 'white';
-    document.getElementById("authors").style.color = 'white';
-
-}
-
-function blue() {
-    document.body.style.backgroundColor = "#b1eed0";
-    document.getElementById("QUICKQUILL").style.color = 'black';
-    document.getElementById("notepicker").style.borderColor = 'black';
-    document.getElementById("note").style.borderColor = 'black';
-    document.getElementById("header").style.borderColor = 'black';
-    document.getElementById("authors").style.color = 'black';
-
-}
-
