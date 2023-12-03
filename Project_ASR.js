afb78b106e9a826b2b85d6d58da2e55bf28de4b9
Autimations <148403069+Autimations@users.noreply.github.com>
2023-12-02 22:08:54 -0500

    Update Project_ASR.js

--- Project_ASR.js
+++ Project_ASR.js
@@ -1,10 +1,10 @@
 /**
  * Toggles between different themes for the application.
  */
-function themePicker() {
-    // Current theme number
-    let themeNumber = 0;
+let themeNumber = 0;
 
+
+function themePicker() {
     // Switch between themes based on the current theme number
     switch (themeNumber) {
         case 0:
@@ -25,6 +25,23 @@ function themePicker() {
             break;
     }
 }
+
+/**
+ * Sets the theme for the application.
+ *
+ * @param {string} bodyColor - The background color for the body.
+ * @param {string} textColor - The text color for various elements.
+ */
+function setTheme(bodyColor, textColor) {
+    // Set the background color of the body
+    document.body.style.backgroundColor = bodyColor;
+
+    // Set text color for specific elements
+    document.getElementById("QUICKQUILL").style.color = textColor;
+    document.getElementById("notepicker").style.borderColor = textColor;
+    document.getElementById("note").style.borderColor = textColor;
+    document.getElementById("header").style.borderColor = textColor;
+    document.getElementById("authors").style.color = textColor;
 
 /**
  * Sets the theme for the application.
