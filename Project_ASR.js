ae3966680e50e0d98d6d9618e3aa96634311e927
Autimations <148403069+Autimations@users.noreply.github.com>
2023-12-02 22:13:47 -0500

    Update Project_ASR.js

--- Project_ASR.js
+++ Project_ASR.js
@@ -42,6 +42,22 @@ function setTheme(bodyColor, textColor) {
     document.getElementById("note").style.borderColor = textColor;
     document.getElementById("header").style.borderColor = textColor;
     document.getElementById("authors").style.color = textColor;
+/**
+ * Sets the theme for the application.
+ *
+ * @param {string} bodyColor - The background color for the body.
+ * @param {string} textColor - The text color for various elements.
+ */
+function setTheme(bodyColor, textColor) {
+    // Set the background color of the body
+    document.body.style.backgroundColor = bodyColor;
+
+    // Set text color for specific elements
+    document.getElementById("QUICKQUILL").style.color = textColor;
+    document.getElementById("notepicker").style.borderColor = textColor;
+    document.getElementById("note").style.borderColor = textColor;
+    document.getElementById("header").style.borderColor = textColor;
+    document.getElementById("authors").style.color = textColor;
 
 /**
  * Sets the theme for the application.
